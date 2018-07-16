d7408abb66d56bdbf21000f451ffa9995a336d8b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-16 17:28:16 -0600

    Test user cannot enter duplicate usernames.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 describe 'Login workflow' do
   it 'allows registered user to login successfully' do
+    user = User.create
   end
 end
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -25,5 +25,32 @@ describe 'Registration workflow' do
   end
 
   it 'submits registration form with duplicate username' do
-    
+    username = 'test'
+    User.create(
+      username: username,
+      password: '54321',
+      name: 'Colin',
+      email: 'colin@email.com',
+      city: 'Denver',
+      role: 0)
+    count = User.count
+
+    visit root_path
+
+    click_on 'Sign Up to be a New User'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: '12345'
+    fill_in :user_name, with: 'Jill'
+    fill_in :user_email, with: 'jill@email.com'
+    fill_in :user_city, with: 'Chicago'
+    fill_in :user_role, with: 0
+
+    click_on 'Create User'
+
+    expect(User.count).to eq(count)
+    expect(current_path).to eq(users_path)
+  end
 end
