d7799bd4292d1c0c99329383626b7583055d5c6c
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-16 18:04:56 -0600

    Validate user cannot create a category that already exists.

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -4,6 +4,7 @@ describe 'User wants to create a new category' do
   it 'successfully fills in required fields' do
     title = 'Sports'
     description = "Athletic endeavors"
+
     visit new_category_path
 
     fill_in :category_title, with: title
@@ -14,5 +15,20 @@ describe 'User wants to create a new category' do
     expect(current_path).to eq(category_path(Category.last))
     expect(page).to have_content(title)
     expect(page).to have_content(description)
+  end
+
+  it 'enters duplicate title' do
+    category = Category.create(title: 'Outdoors', description: 'Fresh air')
+    count = Category.count
+
+    visit new_category_path
+
+    fill_in :category_title, with: category.title
+    fill_in :category_description, with: category.description
+
+    click_on 'Create Category'
+
+    expect(current_path).to eq(categories_path)
+    expect(Category.count).to eq(count)
   end
 end
